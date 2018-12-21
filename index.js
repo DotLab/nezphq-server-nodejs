7154aa0c6d76f134ecb79b6cd0f55f1e6248baa9
Kailang <kailangfu@gmail.com>
2018-12-21 17:37:24 -0600

    Finish

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require("express")();
-const server = require("http").createServer(app);
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-server.listen(80);
-app.get("/", (_, res) => res.status(500).end());
+server.listen(3000, () => console.log('listening on *:3000'));
+// app.get("/", (_, res) => res.status(200).end());
 
 const freeRoomIds = [];
 const rooms = [];
@@ -19,20 +19,82 @@ function getRoom() {
 }
 
 function releaseRoom(room) {
+	rooms[room.id].socket = null;
 	rooms[room.id] = { id: room.id };  // flush room
 	freeRoomIds.push(room.id);
 }
 
 io.on("connection", socket => {
-	const room = getRoom();
-	room.socketId = socket.id;
+	console.log("connection", socket.id);
+	var room;
 
-	socket.emit('sv_id', room.id);
-	
-	socket.on("cl_pub_key", pubKey => {
-		room.pubKey = pubKey;
+	socket.on("cl_handshake", (credentials, fn) => {
+		console.log("\tcl_handshake", socket.id, credentials);
+		if (room || !credentials) {
+			socket.disconnect();
+			return;
+		}
+
+		room = getRoom();
+		room.socket = socket;
+		room.credentials = credentials;
+		room.sin = 0;
+
+		setTimeout(() => {
+			fn(room.id);
+		}, 500)
 	});
-	socket.on("cl_connect", (id, fn) => {
-		
+
+	socket.on("cl_connect_to", (id, fn) => {
+		console.log("\tcl_connect_to", socket.id, id);
+		if (!room || room.connectedRoom || room.sin >= 1) {
+			socket.disconnect();
+			return;
+		}
+
+		//  id exists    id socket exists      id not connected
+		if (rooms[id] && rooms[id].socket && !rooms[id].connectedRoom) {
+			room.waitingId = id;
+
+			if (rooms[id].waitingId === undefined) {  // connect to a free person
+				fn(0);
+			} else if (rooms[id].waitingId === room.id) {  // connect to someone waiting
+				rooms[id].socket.emit("sv_connected", room.id, room.credentials);
+				room.socket.emit("sv_connected", id, rooms[id].credentials);
+	
+				rooms[id].connectedRoom = room;
+				room.connectedRoom = rooms[id];
+				fn(0);
+			} else {  // connect to someone not waiting for you
+				console.log("connect to someone not waiting for you")
+				room.sin += 1;
+				setTimeout(() => {
+					fn(1);
+				}, 1000)
+			}
+		} else {
+			console.log("connect to non-valid room");
+			room.sin += 1;
+			setTimeout(() => {
+				fn(2);
+			}, 1000)
+		}
+	});
+
+	socket.on("cl_send", (msg, fn) => {
+		console.log("\tcl_send", socket.id, msg);
+		if (!room || !room.connectedRoom || room.connectedRoom.ended || typeof msg !== "string") {
+			socket.disconnect();
+			return;
+		}
+
+		room.connectedRoom.socket.emit("sv_deliver", msg);
+		fn(0);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("\tdisconnect", socket.id);
+		if (room) releaseRoom(room);
+		room = null;
 	});
 });
